f592d16b327c2ad130a58fee7a155ae71a00213b
Nathan Ginnever <ginneversource@gmail.com>
2017-05-09 14:32:39 -0700

    Update PublicContainer.js

--- src/containers/PublicContainer.js
+++ src/containers/PublicContainer.js
@@ -13,22 +13,20 @@ export const PublicContainer = React.createClass({
   },
   componentWillMount: function() {
     api.getData(this.props.params.value, this.props.params.value2).then((data) => {
-      console.log('got decrypted data')
-      console.log(data)
+      // got decrypted data
       var blob = new Blob([data], {type:"image/jpg"})
-      //var file = new File(data, "invisible-pogo-stick-cat.jpg", {type:"image/png"});
       this.setState({
         url: window.URL.createObjectURL(blob),
         share: 'http://storj.pics/#/public/'+ this.props.params.value+'/files/'+this.props.params.value2
       })
     })
   },
-	render: function() {
-		return (
+  render: function() {
+    return (
       <Public 
         params={this.props.params} 
         url={this.state.url} 
         share={this.state.share} />
-		)
-	}
+    )
+  }
 })
