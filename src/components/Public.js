83be9e0e86fd42efb1d15f460292d263aad40425
Nathan Ginnever <ginneversource@gmail.com>
2017-05-09 14:38:33 -0700

    Update Public.js

--- src/components/Public.js
+++ src/components/Public.js
@@ -7,20 +7,20 @@ export const Public = React.createClass({
   componentDidMount: function() {
 
   },
-	render: function() {
-		return(
+  render: function() {
+    return(
       <div>
-			<h3 className="seedcenter"> Served from Storj with love</h3>
-       <h4 className="seedcenter">bucket id: {this.props.params.value} </h4>
-       <h4 className="seedcenter">file id: {this.props.params.value2}</h4>
-       <div className="imgcontain">
-       <img className="imgport" src={this.props.url}></img>
-       <p className="seedcenter">Share your photo with the url below</p>
+        <h3 className="seedcenter"> Served from Storj with love</h3>
+        <h4 className="seedcenter">bucket id: {this.props.params.value} </h4>
+        <h4 className="seedcenter">file id: {this.props.params.value2}</h4>
+        <div className="imgcontain">
+          <img className="imgport" src={this.props.url}></img>
+          <p className="seedcenter">Share your photo with the url below</p>
+        </div>
+        <div className="linker">
+          <input className="form-control" value={this.props.share}/>
        </div>
-       <div className="linker">
-       <input className="form-control" value={this.props.share}/>
-       </div>
-       </div>
-		)
-	}
+      </div>
+    )
+  }
 })
